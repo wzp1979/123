49b8507a4e0d550ecc9b1bf3fa1b371e2178a262
julian <julian@julian.com>
2022-03-22 16:01:21 +0800

    chang update

--- Temp.py
+++ Temp.py
@@ -1,13 +1,1 @@
-"""广播"""
-
-from socket import *
-s = socket(AF_INET,SOCK_DGRAM)
-
-s.setsockopt(SOL_SOCKET,SO_BROADCAST, 1)
-
-s.bind(('0.0.0.0',8888))
-
-while True:
-    msg,addr = s.recvfrom(1024)
-    print(msg.decode())
 
